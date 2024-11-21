e005bb573e14b5e5413e212abdd2140fc0249800
aashisarbhai <aashi.sarbhai@gmail.com>
2024-11-21 22:47:35 +0530

    terms and conditions and privacy policy

--- Client/src/Components/Navbar/Navbar.jsx
+++ Client/src/Components/Navbar/Navbar.jsx
@@ -44,7 +44,7 @@ const Navbar = () => {
         <li><a href="/train">Train</a></li>
         <li><a href="/bus">Bus</a></li>
         <li><a href="/CustomerService">Customer Services</a></li>
-        <button className="download-btn"><a href="/signup">Sign Up</a></button>
+        <button className="download-btn"><a href="/signup">Log-In</a></button>
       </ul>
       <div className="menu-icon" onClick={toggleMenu}>
         <div className={`bar ${isOpen ? "bar1" : ""}`}></div>
